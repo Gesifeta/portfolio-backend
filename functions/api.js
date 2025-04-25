b31472fdd799fc374e860fdbf10abbf18efd209a
gesifeta <adamgemechu@gmail.com>
2025-04-25 11:06:03 +0300

    updated toml file

--- functions/api.js
+++ functions/api.js
@@ -2,10 +2,10 @@ import express from "express";
 import ServerlessHttp from "serverless-http";
 
 const api = express();
-api.get("/.netlify/functions/api", (req, res) => {
-  res.send("Hello World!");
+api.get("/api", (req, res) => {
+  res.send("SERVER RUNNING....");
 });
-api.get("/.netlify/functions/api/hello", (req, res) => {
+api.get("/api/hello", (req, res) => {
   res.send("Hello World!");
 });
 export const handler = ServerlessHttp(api);
