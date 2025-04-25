67b67ad2e804ca84f5c8ed44cd0ab272eb92715e
gesifeta <adamgemechu@gmail.com>
2025-04-25 10:59:22 +0300

    Setup Netlify funciton

--- functions/api.js
+++ functions/api.js
@@ -5,4 +5,7 @@ const api = express();
 api.get("/.netlify/functions/api", (req, res) => {
   res.send("Hello World!");
 });
+api.get("/.netlify/functions/api/hello", (req, res) => {
+  res.send("Hello World!");
+});
 export const handler = ServerlessHttp(api);
