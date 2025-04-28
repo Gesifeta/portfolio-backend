b7ff6cbb1f54a738953bc301c538401d0b8f00a7
gesifeta <adamgemechu@gmail.com>
2025-04-28 09:06:20 +0300

    updated upload middleware

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -103,46 +103,5 @@ app.use((req, res, next) => {
   }
   next();
 });
-// Start server
-let server = app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
-// Graceful shutdown handler
-function gracefulShutdown(signal) {
-  console.log(`Received ${signal}. Starting graceful shutdown...`);
-
-  server.close(async () => {
-    console.log("HTTP server closed.");
-    try {
-      // Assuming you have a pool object from your database connection
-      // Add this to your database/db.js file
-      await closePool();
-      console.log("Database pool has ended");
-      process.exit(0);
-    } catch (err) {
-      console.error("Error during shutdown:", err);
-      process.exit(1);
-    }
-  });
-
-  // Force shutdown after 30 seconds
-  setTimeout(() => {
-    console.error(
-      "Could not close connections in time, forcefully shutting down"
-    );
-    process.exit(1);
-  }, 30000);
-}
-
-// Handle different signals for shutdown
-process.on("SIGTERM", () => gracefulShutdown("SIGTERM"));
-process.on("SIGINT", () => gracefulShutdown("SIGINT"));
-
-// Modify your unhandled rejection handler
-process.on("unhandledRejection", (err) => {
-  console.log("UNHANDLED REJECTION! 💥 Shutting down...");
-  console.log(err.name, err.message);
-  gracefulShutdown("UNHANDLED REJECTION");
-});
 
 export const handler = ServerlessHttp(app);
