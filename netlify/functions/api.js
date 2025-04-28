7d41f49c20b9524432e6f978a6c4c91f89ce6823
gesifeta <adamgemechu@gmail.com>
2025-04-28 17:50:12 +0300

    added cors

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -34,7 +34,11 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "https://gemechuadam.com",
+    origin: [
+      "https://gemechuadam.com",
+      "http://localhost:5173",
+      "http://localhost:3000",
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
