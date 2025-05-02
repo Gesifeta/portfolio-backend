063df4acee771a54d5c4f050ab9ca7a115612374
gesifeta <adamgemechu@gmail.com>
2025-05-02 17:41:08 +0300

    added auth middleware to analytics

--- netlify/functions/routes/analytics.router.js
+++ netlify/functions/routes/analytics.router.js
@@ -7,16 +7,17 @@ import {
   trackUserVisitor,
   getUserAnalytics,
 } from "../controllers/analyticsController.js";
+import { isAuthenticated } from "../auth/middleware.js";
 
 export const analyticsRouter = express.Router();
 analyticsRouter.post("/analytics/track", trackUserVisitor);
-analyticsRouter.post(
-  "/analytics/track/country",
-
-  trackCountryPageView
+analyticsRouter.post("/analytics/track/country", trackCountryPageView);
+analyticsRouter.get("/analytics/users", isAuthenticated, getUserAnalytics);
+analyticsRouter.get(
+  "/analytics/track/countries",
+  isAuthenticated,
+  getCountryAnalytics
 );
-analyticsRouter.get("/analytics/users", getUserAnalytics);
-analyticsRouter.get("/analytics/track/countries", getCountryAnalytics);
 analyticsRouter.post(
   "/analytics/track/country/visitor",
 
